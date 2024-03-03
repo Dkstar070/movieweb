5f02691a6919d506fb1579b3f5b295f688b70f0b
Dkstar070 <161037524+Dkstar070@users.noreply.github.com>
2024-03-03 18:39:16 +0530

    Delete movie_script.js

--- movie_script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Retrieve movie name from the URL query parameters
-    const urlParams = new URLSearchParams(window.location.search);
-    const movieName = urlParams.get("name");
-
-    // Display the movie title on the page
-    const movieTitleElement = document.getElementById("movieTitle");
-    movieTitleElement.textContent = movieName;
-});
